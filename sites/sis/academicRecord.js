ce913b405449c7a600035419796dc8c5483317c4
Ruben Caleb <nullorigin.exe@gmail.com>
2025-12-04 09:16:49 +0200

    loading

--- sites/sis/academicRecord.js
+++ sites/sis/academicRecord.js
@@ -9,4 +9,21 @@ fetch(url)
   .then((res) => res.text())
   .then((html) => {
     iframeParent.insertAdjacentHTML("beforeend", html);
+    const generateBtn = document.getElementById("generateBtn");
+    const downloadBtn = document.getElementById("downloadBtn");
+    const resetBtn = document.getElementById("resetBtn");
+
+    generateBtn.addEventListener("click", () => {
+      downloadBtn.removeAttribute("disabled");
+
+      document.getElementById("downloadBtn").addEventListener("click", () => {
+        const link = document.createElement("a");
+        link.href = "..\\..\\assets\\gradebook\\AcademicRecord_42849349.pdf";
+        link.download = "myfile.pdf"; // filename when saved
+        link.style.display = "none";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+    });
   });
